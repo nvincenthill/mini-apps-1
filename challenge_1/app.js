f04ff7c3212044f221fcc8f7be6266d2022b7989
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-09 11:38:13 -0700

    Implement main structure and styling

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,24 @@
+console.log("loading game...");
 
+// board state
+let board = [["T", "I", "C"], ["T", "A", "C"], ["T", "O", "E"]];
 
+let startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", () => {
+  handleClick();
+});
+
+for (let i = 1; i <= 9; i++) {
+  document.getElementById(`square${i}`);
+}
+
+let handleClick = () => {
+  console.log("starting a new game...");
+  let square;
+  for (let i = 1; i <= 9; i++) {
+    square = document.getElementById(`square${i}`);
+    square.innerHTML = "";
+    console.log(square);
+  }
+};
