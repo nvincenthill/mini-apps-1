f56c0c831a3de687045eca25f83cca5d47c76193
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-12 14:31:38 -0700

    Implement gameboard rotation

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -12,15 +12,60 @@ class App extends React.Component {
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0]
-      ]
+      ],
+      isPlaying: true,
+      isInsane: true,
+      currentRotation: 0,
+      nextPiece: 1
     };
+
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick(row, idx) {
+    // alert(`Row ${row} number ${idx} clicked`);
+    if (this.state.isPlaying) {
+      let board = this.state.gameState;
+      if (board[row - 1][idx] === 0) {
+        this.handleNextPiece();
+        board[row - 1][idx] = this.state.nextPiece;
+        this.applyGravity();
+        this.validateBoard();
+        console.table(this.state.gameState);
+        if (this.state.isInsane) {
+          let currentRotation = this.state.currentRotation;
+          this.setState({ currentRotation: currentRotation + 90 });
+        }
+      }
+    }
+  }
+
+  handleNextPiece() {
+    let lastPiece = this.state.nextPiece;
+    if (lastPiece === -1) {
+      this.setState({ nextPiece: 1 });
+    } else {
+      this.setState({ nextPiece: -1 });
+    }
+  }
+
+  applyGravity() {}
+
+  validateBoard() {
+    console.log("validating board");
+  }
+
   render() {
     return (
       <React.Fragment>
-        {/* <h1 id="title">Connect Four</h1> */}
-        <GameBoard gameState={this.state.gameState} />
+        <h1 id="title">CONNECT FOUR</h1>
+        <GameBoard
+          gameState={this.state.gameState}
+          handleClick={this.handleClick}
+          currentRotation={this.state.currentRotation}
+        />
       </React.Fragment>
     );
   }
@@ -31,18 +76,97 @@ class GameBoard extends React.Component {
     super(props);
   }
   render() {
-    let gameBoardLayout = this.props.gameState.map((tile, index) => {
-      return <Tile key={index} />;
+    let row1 = this.props.gameState[0].map((tile, index) => {
+      return (
+        <Tile
+          key={index}
+          gameState={this.props.gameState}
+          handleClick={this.props.handleClick}
+          index={index}
+          row={1}
+        />
+      );
+    });
+    let row2 = this.props.gameState[1].map((tile, index) => {
+      return (
+        <Tile
+          key={index}
+          gameState={this.props.gameState}
+          handleClick={this.props.handleClick}
+          index={index}
+          row={2}
+        />
+      );
+    });
+    let row3 = this.props.gameState[2].map((tile, index) => {
+      return (
+        <Tile
+          key={index}
+          gameState={this.props.gameState}
+          handleClick={this.props.handleClick}
+          index={index}
+          row={3}
+        />
+      );
+    });
+    let row4 = this.props.gameState[3].map((tile, index) => {
+      return (
+        <Tile
+          key={index}
+          gameState={this.props.gameState}
+          handleClick={this.props.handleClick}
+          index={index}
+          row={4}
+        />
+      );
+    });
+    let row5 = this.props.gameState[4].map((tile, index) => {
+      return (
+        <Tile
+          key={index}
+          gameState={this.props.gameState}
+          handleClick={this.props.handleClick}
+          index={index}
+          row={5}
+        />
+      );
+    });
+    let row6 = this.props.gameState[5].map((tile, index) => {
+      return (
+        <Tile
+          key={index}
+          gameState={this.props.gameState}
+          handleClick={this.props.handleClick}
+          index={index}
+          row={6}
+        />
+      );
+    });
+    let row7 = this.props.gameState[6].map((tile, index) => {
+      return (
+        <Tile
+          key={index}
+          gameState={this.props.gameState}
+          handleClick={this.props.handleClick}
+          index={index}
+          row={7}
+        />
+      );
     });
 
+    let rotatedStyle = {
+      transform: `rotate(${this.props.currentRotation}deg)`
+    };
+
     return (
-      <div id="gameboard">
-        {gameBoardLayout}
-        {gameBoardLayout}
-        {gameBoardLayout}
-        {gameBoardLayout}
-        {gameBoardLayout}
-        {gameBoardLayout}
+      <div id="gameboard" style={rotatedStyle}>
+        {row1}
+        {row2}
+        {row3}
+        {row4}
+        {row5}
+        {row6}
+        {row7}
       </div>
     );
   }
@@ -51,9 +175,42 @@ class GameBoard extends React.Component {
 class Tile extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      colorDisplayed: null,
+      needsToRerender: false
+    };
   }
+
+  updateTile() {
+    if (this.props.gameState[this.props.row - 1][this.props.index] === 1) {
+      this.setState({ colorDisplayed: "black" });
+    }
+    if (this.props.gameState[this.props.row - 1][this.props.index] === -1) {
+      this.setState({ colorDisplayed: "red" });
+    }
+  }
+
+  componentWillReceiveProps() {
+    this.updateTile();
+  }
+
+  componentDidMount() {
+    this.updateTile();
+  }
+
   render() {
-    return <div className="tile" />;
+    let RedGamePiece = <div className="token-red" />;
+    let BlackGamePiece = <div className="token-black" />;
+
+    return (
+      <div
+        className="tile"
+        onClick={() => this.props.handleClick(this.props.row, this.props.index)}
+      >
+        {this.state.colorDisplayed === "red" ? RedGamePiece : null}
+        {this.state.colorDisplayed === "black" ? BlackGamePiece : null}
+      </div>
+    );
   }
 }
 
